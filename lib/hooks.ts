b9b4924bc8078cd4ffae817f1451e042a235c85d
John Cido <JohnCido@users.noreply.github.com>
2023-05-02 18:04:41 +0800

    Update hook to use react-use-websocket;

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -1,15 +1,19 @@
 import {
-  SocketEventHandlerMap,
-  LiveAssistantClient,
+  DataType,
+  DataTypeToPayloadMap,
+  PROTOCOL_VERSION,
+  SocketDataPayload,
 } from '@live-assistant/protocols-data'
 import { useRouter } from 'next/router'
 import {
-  useMemo,
   useEffect,
+  useMemo,
 } from 'react'
+import { useWebSocket } from 'react-use-websocket/dist/lib/use-websocket'
 
-export function useSocket(
-  handlers: Partial<SocketEventHandlerMap>,
+export function useSocket<T extends DataType, P = DataTypeToPayloadMap[T]>(
+  type: T,
+  handler: (payload: P) => void,
   timeout?: number,
 ) {
   const {
@@ -19,24 +23,35 @@ export function useSocket(
     },
   } = useRouter()
 
-  const parsedPort = useMemo(() => parseInt(port as string), [port])
+  const parsedPort = useMemo(() => parseInt(port as string ?? '0', 10), [port])
+  const parsedPassword = useMemo(() => password as string | undefined, [password])
 
-  const client = useMemo(() => {
-    return new LiveAssistantClient(
-      parsedPort,
-      password as string,
-      handlers,
-      timeout,
-    )
-  }, [handlers, parsedPort, password, timeout])
+  const url = useMemo(
+    () => `ws://localhost:${parsedPort}/data`,
+    [parsedPort],
+  )
+
+  const shouldConnect = useMemo(
+    () => parsedPort !== 0 && parsedPassword !== undefined,
+    [parsedPassword, parsedPort],
+  )
+
+  const {
+    lastJsonMessage,
+  } = useWebSocket(url, {
+    queryParams: {
+      authorization: parsedPassword ?? '',
+      types: type,
+      version: PROTOCOL_VERSION,
+    },
+    retryOnError: true,
+    reconnectInterval: timeout,
+    shouldReconnect: () => true,
+  }, shouldConnect)
 
   useEffect(() => {
-    if (!isNaN(parsedPort) && password !== undefined) {
-      client.start()
+    if (lastJsonMessage !== null) {
+      handler((lastJsonMessage as SocketDataPayload).payload as P)
     }
-
-    return () => {
-      client.stop()
-    }
-  }, [client, parsedPort, password, port])
+  }, [handler, lastJsonMessage])
 }
